7c2b2319af9d315962f9dd0aee6fc4ed35051094
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-04-13 11:26:41 -0300

    DONE: Requisite 1/7 v1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.__data = list()  # type: ignore
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.__data)  # type: ignore
 
-    def enqueue(self, value):
-        """Aqui irá sua implementação"""
+    def enqueue(self, value):  # type: ignore
+        self.__data.append(value)  # type: ignore
 
-    def dequeue(self):
-        """Aqui irá sua implementação"""
+    def dequeue(self):  # type: ignore
+        # i would like to create a deque to use popleft
+        # but the requisite asks for a list
+        return self.__data.pop(0)  # type: ignore
 
-    def search(self, index):
-        """Aqui irá sua implementação"""
+    def search(self, index):  # type: ignore
+        if 0 <= index < len(self.__data):  # type: ignore
+            return self.__data[index]  # type: ignore
+        raise IndexError("Índice Inválido ou Inexistente")
