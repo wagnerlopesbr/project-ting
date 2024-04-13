02c9978f79ace311e06e221ae48f96b6814aa469
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-04-13 13:32:40 -0300

    DONE: Requisite 8/8 v1

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -23,5 +23,26 @@ def exists_word(word, instance: Queue):  # type: ignore
     return results  # type: ignore
 
 
-def search_by_word(word, instance):  # type: ignore
-    """Aqui irá sua implementação"""
+def search_by_word(word, instance: Queue):  # type: ignore
+    results = []
+
+    for file_data in instance:  # type: ignore
+        occurrences = []
+        file_name = file_data["nome_do_arquivo"]  # type: ignore
+        lines = file_data["linhas_do_arquivo"]  # type: ignore
+
+        for i, line in enumerate(lines, 1):  # type: ignore
+            if word.lower() in line.lower():  # type: ignore
+                occurrences.append({
+                    "linha": i,
+                    "conteudo": line.strip()
+                })  # type: ignore
+
+        if occurrences:
+            results.append({  # type: ignore
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": occurrences
+            })
+
+    return results  # type: ignore
