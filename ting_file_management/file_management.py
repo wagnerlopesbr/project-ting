e475f74e676c686c29263edf124bcaa6b5d9ee30
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-04-13 11:52:11 -0300

    DONE: Requisite 2/7 v1

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file):  # type: ignore
+    if not path_file.endswith(".txt"):  # type: ignore
+        print("Formato inválido", file=sys.stderr)  # type: ignore
+        return []  # type: ignore
+
+    try:
+        with open(path_file, "r") as file:  # type: ignore
+            return file.read().split("\n")
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []  # type: ignore
